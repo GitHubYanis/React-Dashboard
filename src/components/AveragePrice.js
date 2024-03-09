73062b00879619570f832b3306dd28a3bb3bb6fc
GitHubYanis <yanis21p@gmail.com>
2024-03-09 17:14:21 -0500

    implemented averagePrice with the option selected

--- src/components/AveragePrice.js
+++ src/components/AveragePrice.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import styles from './AveragePrice.module.css';
+import data from '../data/database.json';
 
-const AveragePrice = () => {
-    const averagePrice = 90;
+const AveragePrice = ({ selectedSeason, selectedLevel, selectedPass }) => {
+    const filteredData = useMemo(() => {
+        return data.filter(entry => (
+            (!selectedSeason || entry.saison === selectedSeason) &&
+            (!selectedLevel || entry.niveau === selectedLevel) &&
+            (!selectedPass || entry.passe === selectedPass)
+        ));
+    }, [selectedSeason, selectedLevel, selectedPass]);
+
+    const calculateAveragePrice = () => {
+        const dataLength = filteredData.length;
+        if (dataLength === 0) return 0;
+
+        const totalPrices = filteredData.reduce((sum, entry) => sum + entry.prix, 0);
+        return totalPrices / dataLength;
+    };
+
+    const averagePrice = calculateAveragePrice();
 
     return (
         <div className={styles.averagePriceContainer}>
             <h2>Prix Moyen</h2>
             {averagePrice > 0 ? (
-            <p>Le prix moyen est: {averagePrice}$</p>
-            ) : ( <p></p> )}
+                <p>Le prix moyen est: {averagePrice.toFixed(2)}$</p>
+            ) : ( 
+                <p>Aucunes données...</p> 
+            )}
         </div>
     ); 
 };
